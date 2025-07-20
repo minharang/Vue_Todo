811c25aab2935277498a1f6f9b50e0a1c2ed8a9c
lssso <lim.catsfactory@gmail.com>
2025-07-20 20:00:58 +0900

    textField components 작업중

--- src/components/common/TheInputBox.vue
+++ src/components/common/TheInputBox.vue
@@ -1,3 +1,15 @@
-<script setup></script>
-<template></template>
+<script setup>
+
+</script>
+<template>
+<!-- <TextField placeholder="" label="비밀번호" type="password" v-model="inputPwd" hint="" /> -->
+<label for="username" class="sr-only">아이디</label>
+<input
+    type="text"
+    id="username"
+    v-model="username"
+    placeholder="아이디"
+    class="form-input"
+/>
+</template>
 <style scoped></style>
