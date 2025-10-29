57d8e7000c7225417bf7dde8b3796d1ebcbc2635
yong <cheesedoncas@gmail.com>
2025-10-29 16:37:45 +0900

    routes 외 controllers 라우터 적용 npm install module-alias 설치 시 반영됨

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-import express from 'express'  /* 설치: npm install express cors / 실행: npm start 또는 node index.js */
-import cors from 'cors'  /* 설치: npm install express cors */
-import todoModule from './routes/todo.js'
+//require('module-alias/register')  /* module-alias 설치: npm install module-alias / 설정: package.json _moduleAliases 참고 */
+try { require('module-alias/register') } catch (e) { /* module-alias 없으면 무시 */ } /* module-alias 설치: npm install module-alias / 설정: package.json _moduleAliases 참고 */
+const express = require('express')  /* 설치: npm install express cors / 실행: npm start 또는 node index.js */
+const cors = require('cors')  /* 설치: npm install express cors */
+// 삭제예정 : 20251029 src 외 controllers 라우팅 위한 주석 처리
+//const todoModule = ('./routes/todo.js')
+// import express from 'express'  /* 설치: npm install express cors / 실행: npm start 또는 node index.js */
+// import cors from 'cors'  /* 설치: npm install express cors */
+// import todoModule from './routes/todo.js'
 
-const todoRoutes = todoModule?.default || todoModule // CommonJS/ESM 모두 대응
+let todoRoutes
+try {
+  // package.json _moduleAliases에 "@routes": "routes" 설정되어 있으면 사용
+  todoRoutes = require('@routes/todo')
+} catch (err) {
+  // alias가 없으면 상대경로로 fallback
+  todoRoutes = require('./routes/todo')
+}
+// 삭제예정 : 20251029 src 외 controllers 라우팅 위한 주석 처리
+//const todoRoutes = todoModule?.default || todoModule // CommonJS/ESM 모두 대응
 
 const app = express()
 app.use(cors())
@@ -11,11 +26,12 @@ app.use(express.json())
 /** todo routes 연결 추가
  * 클라이언트(웹)의 요청 URL이 /todos로 시작하면 todoRoutes(Express Router)에 위임
  * web에서 POST /todos 요청이 오면 Express는 todoRoutes 내부의 router.post('/') 핸들러를 실행하고, 
- * 그 핸들러에서 DB 쿼리(또는 컨트롤러 호출)를 수행, CRUD는 모두 routes/todo.js 에서 처리 
+ * 그 핸들러에서 DB 쿼리(또는 컨트롤러 호출)를 수행, CRUD는 모두 routes/todo.js => controllers/todoController.js 에서 처리
  */
 app.use('/todos', todoRoutes)
 
 
+/* 삭제예정 : 20251029 src 외 controllers 라우팅 위한 주석 처리
 let todos = []
 let id = 1
 
@@ -43,5 +59,6 @@ app.delete('/todos/:id', (req, res) => {
   console.log(todos)
   res.status(204).end()
 })
+*/
 
 app.listen(3000, () => console.log('API 서버 실행중: http://localhost:3000'))
--- routes/comcd.js
+++ routes/comcd.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const comCdController = require('../controllers/comCdController'); 
+const comCdController = require('@controllers/comCdController'); 
 
 // GET /api/comcd/:grpId
 router.get('/:grpId', comCdController.getComCdByGrpId); 
